9d58db9dd9cdff47ede37cd99df5239fdb5f5ef7
Dandariene <dandariene@hotmail.com>
2022-10-06 09:23:27 -0300

    iniciando os estudos java start capgemini

---
+++ ComandosLeituraGravação/src/main/java/ComandosLeituraGravação/Main.java
@@ -0,0 +1,22 @@
+package ComandosLeituraGravação;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+       Scanner leitor = new Scanner(System.in);
+       
+       int idade = leitor.nextInt();
+       float cotacaoDolar = leitor.nextFloat();
+       double cotacaoEuro = leitor.nextDouble();
+       String nome = leitor.nextLine();
+       String codigoRua = leitor.next();
+       
+        System.out.println("Texto que será exibido");
+        System.out.print("Texto que será exibido");
+    }
+    
+}
---
+++ MeuPrimeiroProjeto/app/src/main/java/MeuPrimeiroProjeto/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MeuPrimeiroProjeto;
+
+/**
+ *
+ * @author 55119
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        System.out.println("Meu segundo comando de saída");
+    }
+    
+}

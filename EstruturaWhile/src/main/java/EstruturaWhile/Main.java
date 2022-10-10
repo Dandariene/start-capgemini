9010607f3852406a19fea91fcebfa260c3c87ad8
Dandariene <dandariene@hotmail.com>
2022-10-10 20:43:03 -0300

    Resolvendo exercicios estrutura while

---
+++ EstruturaWhile/src/main/java/EstruturaWhile/Main.java
@@ -0,0 +1,28 @@
+
+package EstruturaWhile;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int totalAlunos = 10;
+        
+        Scanner leitorScanner = new Scanner(System.in);
+        
+        while(totalAlunos > 0){
+            
+            System.out.println("Nome do aluno: ");
+            String nomeAluno = leitorScanner.nextLine();
+            System.out.println("Idade do aluno: ");
+            int idadeAluno = leitorScanner.nextInt();
+            
+            System.out.println("O nome do aluno é " + nomeAluno + ", a sua idade"
+                    + "é" + idadeAluno);
+            
+            totalAlunos = totalAlunos - 1;
+        }
+    }    
+}

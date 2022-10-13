0d9d71f7c0a444bdab2cbb305555b1c8b33500d8
Dandariene <dandariene@hotmail.com>
2022-10-13 11:30:08 -0300

    exercício praticando estrutura for

--- start-capgemini/src/EstruturaFor/Main.java
+++ start-capgemini/src/EstruturaFor/Main.java
@@ -6,13 +6,16 @@ public class Main {
     public static void main(String[] args) {
 
         String nomeProduto;
-        float precoCusto;
-        float precoVenda;
+        float precoCusto = 0.0f;
+        float precoVenda = 0.0f;
+
+        float totalCusto = 0.0f;
+        float totalVenda = 0.0f;
 
 
         Scanner leitor = new Scanner(System.in);
-
-        for(int i = 0; i < 40; i++){
+        int i = 0;
+        for(; i < 40; i++){
 
             System.out.println("Digite o nome do produto: ");
             nomeProduto = leitor.next();
@@ -23,6 +26,9 @@ public class Main {
             System.out.println("Digite o preço de venda do produto: ");
             precoVenda = leitor.nextFloat();
 
+            totalCusto = totalCusto + precoCusto;
+            totalVenda = totalVenda + precoVenda;
+
             if (precoCusto == precoVenda){
                 System.out.println("Houve um empate de valores");
             }else {
@@ -32,6 +38,10 @@ public class Main {
                     System.out.println("Houve prejuízo" );
                 }
             }
+            System.out.println(nomeProduto + ", preço de custo - " + precoCusto
+                    + ", preço de venda - " + precoVenda);
         }
+        System.out.println("A média do preço de custo é de: " + totalCusto);
+        System.out.println("A média do preço de venda é de: " + totalVenda);
     }
 }
